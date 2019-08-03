a973cea799510c3669d5f9facf9ab0dee14fc5dc
AlexanderChiuluvB <16307110258@fudan.edu.cn>
2019-08-03 11:16:27 +0800

    mapDouble

---
+++ src/main/java/mapDouble/mapDouble.java
@@ -0,0 +1,35 @@
+package mapDouble;
+
+import org.apache.spark.api.java.JavaDoubleRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.sources.In;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class mapDouble {
+    public static void main(String[] args) {
+
+        SparkSession sparkSession = SparkSession.builder().appName("JavaWordCount").master("local").getOrCreate();
+        //spark对普通List的reduce操作
+        JavaSparkContext javaSparkContext = new JavaSparkContext(sparkSession.sparkContext());
+
+        List<Integer> random = Collections.unmodifiableList(
+                new Random()
+                        .ints(-100, 101).limit(100000000)
+                        .boxed()
+                        .collect(Collectors.toList())
+        );
+
+        JavaRDD<Integer> rdd = javaSparkContext.parallelize(random);
+
+        JavaDoubleRDD result = rdd.mapToDouble(x->(double)x*x);
+
+        System.out.println(result.stats().toString());
+
+    }
+}
